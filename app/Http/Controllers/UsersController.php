2de128e391aac07855f9e7d674fcdd8231f69480
mijael <mijaelsaban@gmail.com>
2021-11-17 00:26:16 +0100

    sample code 1 and 2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,9 +15,23 @@ use Illuminate\Validation\ValidationException;
  */
 class UsersController extends Controller
 {
+    /**
+     * @param UserRepository $userRepository
+     * @return mixed
+     */
     public function index(UserRepository $userRepository)
     {
         return $userRepository->getActiveAustrians();
+    }
+
+
+    /**
+     * @param User $user
+     * @return User
+     */
+    public function show(User $user): User
+    {
+        return $user;
     }
 
 
@@ -49,8 +63,7 @@ class UsersController extends Controller
         UserRepository $userRepository
     ): JsonResponse {
         $userRepository->delete($user);
-
-
+        
         return response()->json([
             'message' => 'The user was deleted successfully',
             'user' => $user
